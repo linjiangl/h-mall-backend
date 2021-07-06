c36ffa82b781fb4a1ca08ff01abd3cd59268d24b
jianglin <8257796@qq.com>
2021-07-06 10:08:19 +0800

    优化配置文件

--- config/proxy.ts
+++ config/proxy.ts
@@ -13,10 +13,30 @@ export default {
       changeOrigin: true,
       pathRewrite: { '^': '' },
     },
+    '/backend/': {
+      target: 'http://127.0.0.1:9501',
+      changeOrigin: true,
+      pathRewrite: { '^': '' },
+    },
+    '/public/': {
+      target: 'http://127.0.0.1:9501',
+      changeOrigin: true,
+      pathRewrite: { '^': '' },
+    },
   },
   test: {
     '/api/': {
       target: 'https://preview.pro.ant.design',
+      changeOrigin: true,
+      pathRewrite: { '^': '' },
+    },
+    '/backend/': {
+      target: '',
+      changeOrigin: true,
+      pathRewrite: { '^': '' },
+    },
+    '/public/': {
+      target: 'http://127.0.0.1:9501',
       changeOrigin: true,
       pathRewrite: { '^': '' },
     },
