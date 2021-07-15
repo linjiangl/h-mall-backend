4f247d468125e85507e52224bfd8f7efac5ea271
jianglin <8257796@qq.com>
2021-07-15 18:04:22 +0800

    add big.js

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,17 +1,17 @@
-import NP from 'number-precision';
+import Big from 'big.js';
 
 export const bcadd = (x: number, y: number, scale: number = 0): string => {
-  return NP.plus(x, y).toFixed(scale);
+  return (new Big(x)).plus(new Big(y)).toFixed(scale);
 };
 
 export const bcsub = (x: number, y: number, scale: number = 0): string => {
-  return NP.minus(x, y).toFixed(scale);
+  return (new Big(x)).minus(new Big(y)).toFixed(scale);
 };
 
 export const bcmul = (x: number, y: number, scale: number = 0): string => {
-  return NP.times(x, y).toFixed(scale);
+  return (new Big(x)).times(new Big(y)).toFixed(scale);
 };
 
 export const bcdiv = (x: number, y: number, scale: number = 0): string => {
-  return NP.divide(x, y).toFixed(scale);
+  return (new Big(x)).div(new Big(y)).toFixed(scale);
 };
